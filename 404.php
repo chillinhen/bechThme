677a7be57e0a3a902054d1f135313131ba071d8a
chillinhen <c.hillen@gmx.net>
2016-07-04 08:40:53 +0200

    final todos

---
+++ 404.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <title><?php wp_title(''); ?><?php
+            if (wp_title('', false)) {
+                echo ' :';
+            }
+            ?> <?php bloginfo('name'); ?></title>
+
+        <link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php get_template_part('inc/meta');?>
+
+        <?php wp_head(); ?>
+        <script>
+            // conditionizr.com
+            // configure environment tests
+            conditionizr.config({
+                assets: '<?php echo get_template_directory_uri(); ?>',
+                tests: {}
+            });
+        </script>
+
+    </head>
+    <?php if (is_front_page()) : ?>
+        <?php if (get_theme_mod('bechhold_BG')) : ?>
+            <body <?php body_class(); ?> style="background-image: url(<?php echo esc_url(get_theme_mod('bechhold_BG')); ?>)">
+            <?php endif; ?>
+        <?php else : ?>
+            <body>
+        <?php endif; ?>
+
+        <?php get_template_part('svg/inline', 'icons.svg'); ?>
+        <!-- header -->
+        <header id="header" class="row" role="banner">
+            <div class="container">
+                <div class="home-btn mobile-layout">
+                    <a href="<?php echo home_url(); ?>" title="<?php echo get_bloginfo('name'); ?>">
+                        <svg><use xlink:href="#homeBtn"></use></svg>
+                    </a>          
+                </div>
+                <!-- nav -->
+                <nav class="nav" role="navigation">
+                    <?php html5blank_nav(); ?>
+                </nav>
+                <!-- /nav -->
+                <div class="navbar-toggle">
+                    <svg><use xlink:href="#menu"></use></svg>
+                </div>
+                <!-- logo -->
+
+                    <h1 class="logo text-right">
+                        <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>">
+                            <span class="name"><?php bloginfo('name'); ?></span>
+                            <span class="description"><?php bloginfo('description'); ?></span>
+                        </a>
+                    </h1>
+
+                <!-- /logo -->
+            </div>
+        </header>
+        <!-- /header -->
+        <main role="main" class="row">
+
+
+<!-- section -->
+<section class="404">
+
+			<!-- article -->
+			<article id="post-404" class="container">
+                             
+
+                                 <h1>
+                                     <?php _e( 'Page not found', 'html5blank' ); ?><br/>
+                                     <span><a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a></span>
+                                 </h1>
+
+			</article>
+			<!-- /article -->
+
+		</section>
+		<!-- /section -->
+
+<?php get_footer(); ?>
--- snippet.php
+++ snippet.php
@@ -1,15 +1,4 @@
+<img src="http://bech.clients.overflow-hillen.de/wp-content/uploads/sites/2/2016/02/DSC_0638_sw-500x645.jpg" class="attachment-large size-large wp-post-image" alt="DSC_0638_sw" draggable="false">
 
-        <?php if (is_page_template('page-home.php')) : ?>
-    <div class="container keywords"><?php the_field('footer-keywords'); ?>
-            <?php elseif (is_page_template('page-contact.php')) :
-            get_template_part('partials/impressum');
-            ?>
-            <p class="copyright container">
-                <?php get_template_part('partials/copyright','text');?>
-            </p>
-<?php else : ?>
-            <p class="copyright container">
-                 <?php get_template_part('partials/copyright','text');?>
-            </p>
-<?php endif; ?>
-    </div>
+
+<img src="small.jpg" srcset="small.jpg 320w, medium.jpg 600w, large.jpg 900w" alt="">
